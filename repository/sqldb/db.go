14b3347ae3725f2646c976a30be9f7d9e97975f9
Masood Keshvari <keshvari.developer@gmail.com>
2023-03-25 22:11:40 +0330

    create tables if not exists

--- repository/sqldb/db.go
+++ repository/sqldb/db.go
@@ -10,6 +10,8 @@ import (
 	"time"
 )
 
+var conf *config.Config
+
 func init() {
 	// loads values from .env into the system
 	if err := godotenv.Load(); err != nil {
@@ -22,13 +24,16 @@ type MySQLDB struct {
 }
 
 func New() *MySQLDB {
-	conf := config.New()
+	conf = config.New()
+
 	dataSourceName := fmt.Sprintf("%s:%s@(%s:%s)/%s?parseTime=true", conf.SqlDB.Username, conf.SqlDB.Password,
 		conf.SqlDB.Host, conf.SqlDB.Port, conf.SqlDB.Name)
+
 	db, err := sql.Open("mysql", dataSourceName)
 	if err != nil {
 		panic(fmt.Errorf("can't open mysql db: %v", err))
 	}
+	initDB(db)
 
 	// See "Important settings" section.
 	db.SetConnMaxLifetime(time.Minute * 3)
@@ -36,4 +41,66 @@ func New() *MySQLDB {
 	db.SetMaxIdleConns(10)
 
 	return &MySQLDB{db: db}
+}
+
+func initDB(db *sql.DB) {
+	//ctx := context.Background()
+
+	//_, cErr := db.Exec("create database if not exists " + conf.SqlDB.Name)
+	//if cErr != nil {
+	//	panic(fmt.Errorf("Error %w when creating DB\n", cErr))
+	//}
+	//_, err := db.ExecContext(ctx, "USE "+conf.SqlDB.Name)
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	createUserTableQuery := `CREATE TABLE IF NOT EXISTS users(
+  id INT NOT NULL AUTO_INCREMENT,
+  email VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE INDEX id_UNIQUE (id ASC) VISIBLE,
+  UNIQUE INDEX email_UNIQUE (email ASC) VISIBLE)`
+
+	_, cErr := db.Exec(createUserTableQuery)
+	if cErr != nil {
+		panic(fmt.Errorf("Error %w when creating DB\n", cErr))
+	}
+
+	createProfileTableQuery := `CREATE TABLE IF NOT EXISTS profiles(
+  id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  name VARCHAR(45) NULL,
+  special_days_notify_active TINYINT NOT NULL,
+  birthday_notify_active TINYINT NOT NULL,
+  vip_active TINYINT NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE INDEX id_UNIQUE (id ASC) VISIBLE,
+  UNIQUE INDEX user_id_UNIQUE (user_id ASC) VISIBLE,
+      FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE)`
+
+	_, cErr = db.Exec(createProfileTableQuery)
+	if cErr != nil {
+		panic(fmt.Errorf("Error %w when creating DB\n", cErr))
+	}
+
+	createPartnerTableQuery := `CREATE TABLE IF NOT EXISTS  partners(
+  id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  name VARCHAR(45) NULL,
+  birthday DATETIME NULL,
+  first_date DATETIME NOT NULL,
+  is_deleted TINYINT NULL,
+  PRIMARY KEY (id),
+  UNIQUE INDEX id_UNIQUE (id ASC) VISIBLE,
+   FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE)`
+	_, cErr = db.Exec(createPartnerTableQuery)
+	if cErr != nil {
+		panic(fmt.Errorf("Error %w when creating DB\n", cErr))
+	}
+
 }
